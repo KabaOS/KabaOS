4c3527deb0c1f4bbd68e768aec9eb829e4c779f9
Arthur Melton <git-zl9limki@dontrackme.com>
2024-02-19 14:51:45 -0500

    Try and get everything to use socks proxy

--- init/post_init.sh
+++ init/post_init.sh
@@ -2,5 +2,7 @@ clear
 rm -f /home/Cloak/.profile
 
 export XDG_CURRENT_DESKTOP=GNOME
+export ALL_PROXY=socks5://127.0.0.1:4447
+export all_proxy=socks5://127.0.0.1:4447
 
 startx /usr/bin/gnome-shell --x11 &>/dev/null
