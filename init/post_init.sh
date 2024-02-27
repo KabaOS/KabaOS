6607062f0fe5b4d9ab45deda50f3efbc01040af7
Arthur Melton <git-zl9limki@dontrackme.com>
2024-02-27 18:45:42 -0500

    Dont specify proxies
    will manually setup all programs to use proxies

--- init/post_init.sh
+++ init/post_init.sh
@@ -2,7 +2,5 @@ clear
 rm -f /home/Kaba/.profile
 
 export XDG_CURRENT_DESKTOP=GNOME
-export ALL_PROXY=socks5://127.0.0.1:4447
-export all_proxy=socks5://127.0.0.1:4447
 
 startx /usr/bin/gnome-shell --x11 &>/dev/null
