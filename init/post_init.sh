35a19219010151e36cd412d35556a1a67f318923
Arthur Melton <git-zl9limki@dontrackme.com>
2024-03-01 12:54:46 -0500

    Disable hardware acceleration

--- init/post_init.sh
+++ init/post_init.sh
@@ -5,4 +5,4 @@ export XDG_CURRENT_DESKTOP=GNOME
 export XDG_SESSION_TYPE=x11
 export GDK_BACKEND=x11
 
-startx /usr/bin/gnome-session &>/dev/null
+startx /usr/bin/gnome-session --disable-acceleration-check &>/dev/null
